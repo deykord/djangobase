bfa2a08f651927eb5350bd5e6519daabe3e93414
Say10 <146481584+deykord@users.noreply.github.com>
2024-08-26 03:54:40 +0330

    transfered an html template in proejct
    copy static and media files of an html tempalte into project and fixing address of static sources in html files

--- website/views.py
+++ website/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
 def index_view(request):
-    return render(request,'website\index.html')
+    return render(request,'website/index.html')
+
 
 def about_view(request):
-    return render(request,'website\about.html')
+    return render(request,"website/about.html")
+
+
 
 def contact_view(request):
-    return render(request,'website\contact.html')
+    return render(request,'website/contact.html')
 
 
