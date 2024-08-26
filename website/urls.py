ab4f7cdb4b48ea27620bf35f4f6ff93fbe7c0d70
Say10 <146481584+deykord@users.noreply.github.com>
2024-08-26 04:20:54 +0330

    added base.html
    moved repeatable codes to a new file named base.html and used in  html pages using extends in python

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
 from website.views import *
-
+app_name = 'website'
 urlpatterns = [
     path('',index_view ),
-    path('index.html',index_view ),
-    path('about.html', about_view ),
-    path('contact.html', contact_view )
+    path('index.html',index_view ,name='home'),
+    path('about', about_view,name='about' ),
+    path('elements.html', elements_view ,name='elements'),
+    path('contact.html', contact_view , name='contact' )
 ]
